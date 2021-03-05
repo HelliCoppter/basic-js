18f4020c15f283bd028947fa6985953b32332efe
Alexander Fufachev <alexfufachev@mail.ru>
2021-03-05 13:26:35 +0300

    feat: pulled last changes and fixed carbon-daiting

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,12 +1,7 @@
-const CustomError = require("../extensions/custom-error");
-
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
 module.exports = function dateSample(sampleActivity) {
 
-  const MODERN_ACTIVITY= 15; 
-  const HALF_LIFE_PERIOD= 5730;
+  const MODERN_ACTIVITY = 15; 
+  const HALF_LIFE_PERIOD = 5730;
   let number = 0;
 
   if (typeof sampleActivity === 'string') {
@@ -15,7 +10,7 @@ module.exports = function dateSample(sampleActivity) {
       if (number >= MODERN_ACTIVITY || number <= 0 || isNaN(number)) {
         return false
       } else {
-        return Math.ceil(MODERN_ACTIVITY / number) / (0.693 / HALF_LIFE_PERIOD);
+        return Math.ceil(((Math.log(MODERN_ACTIVITY / number)) / (Math.log(2)) * HALF_LIFE_PERIOD));
       }
     
   } else return false
